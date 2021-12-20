afd6908057929bd5d0a54a77a474bfec179058ce
Robert <rromanowicz@hotmail.com>
2021-12-20 08:16:52 +0100

    files to previous commit

--- app/src/main/java/ex/rr/archerysession/SharedViewModel.kt
+++ app/src/main/java/ex/rr/archerysession/SharedViewModel.kt
@@ -3,16 +3,35 @@ package ex.rr.archerysession
 import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import ex.rr.archerysession.data.DbSession
+import ex.rr.archerysession.data.Session
 
 class SharedViewModel : ViewModel() {
     var scores = MutableLiveData<MutableList<Int>?>()
+    var session = MutableLiveData<Session?>()
+    var dbSession = MutableLiveData<DbSession?>()
+    var sessionId = MutableLiveData<String?>()
 
     fun sendScores(values: MutableList<Int>) {
         Log.e("ViewModel", values.toString()) //TODO: remove
         scores.value = values
     }
 
+    fun sendSession(value: Session) {
+        session.value = value
+    }
+
+    fun sendDbSession(value: DbSession) {
+        dbSession.value = value
+    }
+
+    fun sendSessionId(value: String) {
+        sessionId.value = value
+    }
+
     fun clear() {
         scores.value?.clear()
+        session.value = null
+        sessionId.value = null
     }
 }
