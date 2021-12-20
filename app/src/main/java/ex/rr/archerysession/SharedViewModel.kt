9c4a36a9425096d80a50c852d69a7b1f85649872
Robert <rromanowicz@hotmail.com>
2021-12-20 08:54:33 +0100

    failing build fix

--- app/src/main/java/ex/rr/archerysession/SharedViewModel.kt
+++ app/src/main/java/ex/rr/archerysession/SharedViewModel.kt
@@ -3,13 +3,13 @@ package ex.rr.archerysession
 import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import ex.rr.archerysession.data.DbSession
+//import ex.rr.archerysession.data.DbSession
 import ex.rr.archerysession.data.Session
 
 class SharedViewModel : ViewModel() {
     var scores = MutableLiveData<MutableList<Int>?>()
     var session = MutableLiveData<Session?>()
-    var dbSession = MutableLiveData<DbSession?>()
+//    var dbSession = MutableLiveData<DbSession?>()
     var sessionId = MutableLiveData<String?>()
 
     fun sendScores(values: MutableList<Int>) {
@@ -21,9 +21,9 @@ class SharedViewModel : ViewModel() {
         session.value = value
     }
 
-    fun sendDbSession(value: DbSession) {
-        dbSession.value = value
-    }
+//    fun sendDbSession(value: DbSession) {
+//        dbSession.value = value
+//    }
 
     fun sendSessionId(value: String) {
         sessionId.value = value
