61481bb7cae00b59c6f2ae9633d22a3a2a110f13
Robert <rromanowicz@hotmail.com>
2021-12-21 14:52:05 +0100

    History fragment shared session_details layout

--- app/src/main/java/ex/rr/archerysession/MainFragment.kt
+++ app/src/main/java/ex/rr/archerysession/MainFragment.kt
@@ -1,16 +1,19 @@
 package ex.rr.archerysession
 
+import android.content.Context
 import android.os.Bundle
 import android.util.TypedValue
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.rr.archerysession.R
 import com.rr.archerysession.databinding.FragmentMainBinding
 import ex.rr.archerysession.db.DBHelper
 import java.text.SimpleDateFormat
+import java.util.*
 
 class MainFragment : Fragment() {
 
@@ -28,16 +31,26 @@ class MainFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val formatter = SimpleDateFormat("dd-MMM-yyyy HH:mm:ss")
+        val inflater = context!!.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
 
         val db = DBHelper(requireContext(), null)
         val lastSession = db.getXSessions("1")[0]
 
-        binding.sessionId.text = lastSession.id.toString()
-        binding.startDate.text = formatter.format(lastSession.session.startDate)
-        binding.endDate.text = formatter.format(lastSession.session.endDate!!)
-        binding.endCount.text = lastSession.session.ends.toString()
-        binding.arrowCount.text = lastSession.session.arrows.toString()
+        val sessionDetails: View = inflater.inflate(R.layout.session_details, null)
+        sessionDetails.findViewById<TextView>(R.id.sessionId).text = lastSession.id.toString()
+        sessionDetails.findViewById<TextView>(R.id.startDate).text =
+            formatter.format(lastSession.session.startDate)
+        sessionDetails.findViewById<TextView>(R.id.endDate).text =
+            formatter.format(lastSession.session.endDate!!)
+        sessionDetails.findViewById<TextView>(R.id.endCount).text =
+            lastSession.session.ends.toString()
+        sessionDetails.findViewById<TextView>(R.id.arrowCount).text =
+            lastSession.session.arrows.toString()
+        sessionDetails.findViewById<TextView>(R.id.avgArrowValue).text =
+            lastSession.session.scores.sumOf { t -> t.sumOf { x -> x } }
+                .div(lastSession.session.arrows)
+                .toString()
+
         var i = 0
         lastSession.session.scores.forEach { score ->
             val formattedScores: MutableList<String> = mutableListOf()
@@ -52,13 +65,17 @@ class MainFragment : Fragment() {
             )
             textView.text =
                 ("${++i}: [${if (endSum.toString().length == 1) "0$endSum" else "$endSum"}] \t$formattedScores")
-            binding.mainScrollView.addView(textView)
+            sessionDetails.findViewById<LinearLayout>(R.id.mainScrollView).addView(textView)
         }
-
+        binding.mainLinearLayout.addView(sessionDetails)
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    companion object {
+        val formatter = SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.ENGLISH)
     }
 }
